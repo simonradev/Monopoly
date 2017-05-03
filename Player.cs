696cdbd0c8ac3bb24a79c806680ab41c27fa63eb
simonradev <simonradev@gmail.com>
2017-05-03 16:39:19 +0300

    Edited the code.

---
+++ Player.cs
@@ -0,0 +1,60 @@
+﻿namespace MonopolyField
+{
+    using System;
+    public class Player
+    {
+        private int totalMoney;
+        private int moneyIncomePerTurn;
+        private int turnsTook;
+
+        public Player()
+        {
+            this.totalMoney = 50;
+            this.moneyIncomePerTurn = 0;
+            this.turnsTook = 0;
+        }
+
+        public void TakeATurn()
+        {
+            this.turnsTook++;
+
+            this.totalMoney += moneyIncomePerTurn;
+        }
+
+        public string BuyAHotel()
+        {
+            this.moneyIncomePerTurn += 10;
+
+            string messageResult = $"Bought a hotel for {this.totalMoney}. Total hotels: {this.moneyIncomePerTurn / 10}.";
+
+            this.totalMoney = 0;
+
+            return messageResult;
+        }
+
+        public string GoToJail()
+        {
+            this.TakeATurn();
+            this.TakeATurn();
+
+            return $"Gone to jail at turn {this.turnsTook - 2}.";
+        }
+
+        public string GoToShop(int row, int col)
+        {
+            row++;
+            col++;
+
+            int moneyToSpend = this.totalMoney >= row * col ? row * col : this.totalMoney;
+
+            this.totalMoney -= moneyToSpend;
+
+            return $"Spent {moneyToSpend} money at the shop.";
+        }
+
+        public string GetPlayerInfo()
+        {
+            return $"Turns {this.turnsTook}{Environment.NewLine}Money {this.totalMoney}";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-    
+
     public class Startup
     {
         private static char[][] monopolyField;
@@ -96,63 +96,6 @@
                     monopolyField[row][col] = currentMonopolyLine[col];
                 }
             }
-        }
-    }
-
-    public class Player
-    {
-        private int totalMoney;
-        private int moneyIncomePerTurn;
-        private int turnsTook;
-
-        public Player()
-        {
-            this.totalMoney = 50;
-            this.moneyIncomePerTurn = 0;
-            this.turnsTook = 0;
-        }
-
-        public void TakeATurn()
-        {
-            this.turnsTook++;
-
-            this.totalMoney += moneyIncomePerTurn;
-        }
-
-        public string BuyAHotel()
-        {
-            this.moneyIncomePerTurn += 10;
-
-            string messageResult = $"Bought a hotel for {this.totalMoney}. Total hotels: {this.moneyIncomePerTurn / 10}.";
-
-            this.totalMoney = 0;
-
-            return messageResult;
-        }
-
-        public string GoToJail()
-        {
-            this.TakeATurn();
-            this.TakeATurn();
-
-            return $"Gone to jail at turn {this.turnsTook - 2}.";
-        }
-
-        public string GoToShop(int row, int col)
-        {
-            row++;
-            col++;
-
-            int moneyToSpend = this.totalMoney >= row * col ? row * col : this.totalMoney;
-
-            this.totalMoney -= moneyToSpend;
-
-            return $"Spent {moneyToSpend} money at the shop.";
-        }
-
-        public string GetPlayerInfo()
-        {
-            return $"Turns {this.turnsTook}{Environment.NewLine}Money {this.totalMoney}";
         }
     }
 }
